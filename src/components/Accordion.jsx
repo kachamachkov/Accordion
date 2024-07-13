f91cc54f6927522734a1109c3d9fb5cd0d80d419
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-13 15:43:50 +0300

    Add clicking functionality to show answer of a question

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,3 +1,39 @@
+import { useState } from 'react';
+import data from './data';
+
 export default function Accordion() {
-  return;
+    const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId);
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="accordion">
+                {data && data.length > 0 ? (
+                    data.map((dataItem) => (
+                        <div className="item" key={dataItem.id}>
+                            <div
+                                onClick={() =>
+                                    handleSingleSelection(dataItem.id)
+                                }
+                                className="title"
+                            >
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {selected === dataItem.id ? (
+                                <div className="content">
+                                    {dataItem.answer}{' '}
+                                </div>
+                            ) : null}
+                        </div>
+                    ))
+                ) : (
+                    <div>No data found!</div>
+                )}
+            </div>
+        </div>
+    );
 }
