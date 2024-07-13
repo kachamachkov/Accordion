dd4891b4fcb356c79abc91e70e2d78ffd3106302
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-13 16:31:11 +0300

    Add single accordion functionality

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -5,14 +5,24 @@ const Accordion = () => {
 
 	const [selected, setSelected] = useState(null);
 
+	const handleQuestionClick = (questionId) => {
+		setSelected(questionId);
+	};
+
 	return (
 		<div>
 			{
 				data && data.length > 0 ?
 					data.map(item =>
 						<div key={item.id}>
-							<h2 >{item.question}</h2>
-							<span>+</span>
+							<h2 onClick={() => handleQuestionClick(item.id)}>{item.question}</h2>
+							<span onClick={() => handleQuestionClick(item.id)}>+</span>
+							<div>
+								{selected === item.id ?
+									item.answer
+									: null
+								}
+							</div>
 						</div>
 					)
 					:
