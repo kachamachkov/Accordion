209565b3667ac3a89da700dae4b3c498c97ec56c
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-16 17:04:08 +0300

    Redo dynamic rendering of single modal

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,39 +1,35 @@
 import { useState } from "react";
 import data from "./data";
-import './styles.css'
+import './styles.css';
 
 const Accordion = () => {
 
-	const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(null);
 
-	function handleSingleSelection(getCurrentId) {
-		// set clicked title ID to selected
-		// add click switching
-		setSelected(getCurrentId === selected ? null : getCurrentId);
-	}
+  const handleTitleClick = (id) => {
+    setSelected(id === selected ? null : id);
+  };
 
-	return (
-		<div className="wrapper">
-			<div className="accordion">
-				{
-					data && data.length > 0 ?
-						data.map(dataItem =>
-							<div className="item" key={dataItem.id}>
-								<div className="title" onClick={() => handleSingleSelection(dataItem.id)}>
-									<h3>{dataItem.question}</h3>
-									<span>+</span>
-								</div>
-								{
-									selected === dataItem.id ?
-										(<div className="content"> {dataItem.answer} </div>)
-										: null
-								}
-							</div>)
-						: <div>No data found!</div>
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {
+          data && data.length > 0 ? data.map(dataItem =>
+            <div className="item" key={dataItem.id}>
+              <h3 className="title" onClick={() => handleTitleClick(dataItem.id)}>{dataItem.question}</h3>
+              <span onClick={() => handleTitleClick(dataItem.id)}>+</span>
+              {
+                selected === dataItem.id ?
+                  (<div className="content">{dataItem.answer}</div>)
+                  : null
+              }
+            </div>
+          ) : <div>No data found</div>
+        }
 
-				}
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
+
 };
 export default Accordion;
