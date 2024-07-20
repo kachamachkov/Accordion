b8aeefe82ae51fbc7b68a8ade60f6f5700014e2d
kachamachkov <kachamachkovboyan@gmail.com>
2024-07-20 15:25:12 +0300

    Finish accordion styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import FAQ from "./components/FAQ";
 
 function App() {
 
     return (
-        <>
-            <h1 className="font-bold text-2xl">Hello</h1>
-        </>
+        <div className="w-full h-screen bg-gradient-to-r from-indigo-500 to-blue-600">
+            <FAQ />
+        </div>
     );
 }
 
---
+++ src/components/FAQ.jsx
@@ -0,0 +1,12 @@
+import Accordion from "./Accordion";
+
+const FAQ = () => {
+    return (
+        <div className="p-4 bg-gray-200 rounded-lg">
+            <Accordion title='Do you prefer Android or IOS?' answer='I like to use IOS' />
+            <Accordion title='Do you prefer writing CSS or Tailwind?' answer='I like to use Tailwind' />
+            <Accordion title='Firebase or Supabase?' answer='I prefer Firebase' />
+        </div>
+    );
+};
+export default FAQ;
